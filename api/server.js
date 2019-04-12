fa65ad8232e248c236d5a265177202e0db52b796
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-12 11:58:16 -0400

    Adds a POST request handler to the server.js file and creates a server.spec.js file for testing

--- api/server.js
+++ api/server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 
-const games = ["Assassin's Creed III", "Gears of War II", "Super Mario World 3", "NBA JAM"];
+const games = require('../games/gamesModel.js');
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-  res.status(200).json({ api: "It's working!!!" });
+server.get('/api', async (req, res) => {
+    res.status(200).json({ api: "It's working!!!" });
 });
 
-server.get('/games', async (req, res) => {
-  const rows = await games.getAll();
+server.get('/api/games', async (req, res) => {
+    const rows = await games.getAll();
 
-  res.status(200).json(rows);
+    res.status(200).json(rows);
+});
+
+server.post('/api/games', async (req, res) => {
+    try {
+      const [id] = await db('games').insert(req.body);
+  
+      const role = await db('games')
+        .where({ id })
+        .first();
+  
+      res.status(201).json(role);
+    } catch (error) {
+      const message = errors[error.errno] || 'We ran into an error trying to add a game to the database';
+      res.status(500).json({ message, error });
+    }
 });
 
 module.exports = server;

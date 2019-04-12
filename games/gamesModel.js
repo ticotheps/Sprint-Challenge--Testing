a43babb929ea0e6cd759d7b18868507a99be3a50
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-12 12:12:15 -0400

    Creates a new 'gamesModel.js' file inside of a new 'games' folder to store our functions that will apply to the database

---
+++ games/gamesModel.js
@@ -0,0 +1,33 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  findById,
+};
+
+async function insert(game) {
+  const [id] = await db('games').insert(game);
+
+  return db('games')
+    .where({ id })
+    .first();
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return null;
+}
+
+function getAll() {
+  return db('games');
+}
+
+function findById(id) {
+  return null;
+}
